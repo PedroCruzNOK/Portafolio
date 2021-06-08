aefce38755bc3498f63ab5c78e4dbe024c0a831d
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-06-08 12:59:03 -0500

    estructura de navegacion

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,6 @@
+<div class="Sidebar">
+    <h4>
+        Noticias
+    </h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, consequuntur? Accusantium odit eos placeat odio eius. Provident omnis a deleniti numquam at recusandae maiores enim dolor velit quaerat, fugit ex.</p>
+</div>
---
+++ src/routes/contact.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Contacto</title>
+</svelte:head>
+
+<div class="Contact">
+    <h2>Contacto</h2>
+    <p>Informacion de contacto</p>
+</div>
+
